d45ddd114df78e4001e6ba513a4bca743432cb24
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2024-12-25 19:42:46 +0100

    Map functionality tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,41 @@
-function App () {
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const customIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // URL de la imagen del ícono
+  iconSize: [32, 32], // Tamaño del ícono [ancho, alto]
+  iconAnchor: [16, 32], // Punto del ícono que se anclará al mapa [x, y]
+  popupAnchor: [0, -32] // Punto donde aparecerá el popup [x, y]
+})
+
+function ClickableMarker() {
+  const [position, setPosition] = React.useState(null)
+
+  useMapEvent('click', (e) => {
+    console.log(e.latlng)
+    setPosition(e.latlng)
+  })
+  return position === null
+    ? null
+    : (
+      <Marker position={position} icon={customIcon}>
+        <Popup>
+          Marker is at {position.toString()}
+        </Popup>
+      </Marker>
+      )
+}
+
+function App() {
   return (
-    <>
-      <h1>Veloway</h1>
-    </>
+    <MapContainer center={[37.18817, -3.60667]} zoom={13} style={{ height: '98vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <ClickableMarker />
+    </MapContainer>
   )
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  cacheDir: 'node_modules/.vite',
+  plugins: [react()],
+  resolve: {
+    preserveSymlinks: true
+  },
+  root: process.cwd()
 })
