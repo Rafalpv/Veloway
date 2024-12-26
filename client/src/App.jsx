5c04eb5df7c0c7a7e429d65cf996112a78a88524
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2024-12-26 18:05:00 +0100

    Add leaflet-routing-machine plugin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import 'leaflet-routing-machine'
+import L from 'leaflet'
 
 const customIcon = L.icon({
   iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // URL de la imagen del ícono
@@ -9,32 +11,61 @@ const customIcon = L.icon({
   popupAnchor: [0, -32] // Punto donde aparecerá el popup [x, y]
 })
 
-function ClickableMarker() {
-  const [position, setPosition] = React.useState(null)
+const startPoint = [37.18817, -3.60667] // Punto inicial (Granada)
+const mediunPoint = [37.1821, -3.60367] // Punto inicial (Granada)
+const endPoint = [37.166, -3.60] // Punto final
 
-  useMapEvent('click', (e) => {
-    console.log(e.latlng)
-    setPosition(e.latlng)
-  })
-  return position === null
-    ? null
-    : (
-      <Marker position={position} icon={customIcon}>
-        <Popup>
-          Marker is at {position.toString()}
-        </Popup>
-      </Marker>
-      )
+function Routing() {
+  const map = useMap() // Obtener la instancia del mapa desde el contexto de React-Leaflet
+
+  React.useEffect(() => {
+    if (!map) return
+
+    // Crear el control de rutas
+    const routingControl = L.Routing.control({
+      waypoints: [
+        L.latLng(startPoint), // Punto inicial
+        L.latLng(mediunPoint), // Punto intermedio
+        L.latLng(endPoint) // Punto final
+      ],
+      routeWhileDragging: true,
+      lineOptions: {
+        styles: [{ color: '#F47384', weight: 4 }]
+      },
+      createMarker: function() {
+        return null
+      }
+    }).addTo(map)
+
+    return () => {
+      map.removeControl(routingControl) // Limpiar control al desmontar
+    }
+  }, [map])
+
+  return null
 }
 
 function App() {
   return (
-    <MapContainer center={[37.18817, -3.60667]} zoom={13} style={{ height: '98vh', width: '100%' }}>
+    <MapContainer
+      center={startPoint}
+      zoom={14}
+      style={{ height: '98vh', width: '100%' }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <ClickableMarker />
+      <Marker position={startPoint} icon={customIcon}>
+        <Popup>Punto de inicio</Popup>
+      </Marker>
+      <Marker position={mediunPoint} icon={customIcon}>
+        <Popup>Punto de inicio</Popup>
+      </Marker>
+      <Marker position={endPoint} icon={customIcon}>
+        <Popup>Punto de fin</Popup>
+      </Marker>
+      <Routing />
     </MapContainer>
   )
 }
