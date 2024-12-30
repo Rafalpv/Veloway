ebe1b0f13730475c33f23b0916877f38f43a56ee
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2024-12-30 12:52:48 +0100

    Add icons and login page

---
+++ client/src/assets/icons.jsx
@@ -0,0 +1,27 @@
+export function MaximizeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e8eaed"
+    >
+      <path d="M120-120v-320h80v184l504-504H520v-80h320v320h-80v-184L256-200h184v80H120Z" />
+    </svg>
+  )
+}
+
+export function MinimizeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e8eaed"
+    >
+      <path d="m136-80-56-56 264-264H160v-80h320v320h-80v-184L136-80Zm344-400v-320h80v184l264-264 56 56-264 264h184v80H480Z" />
+    </svg>
+  )
+}
---
+++ client/src/pages/Map.jsx
@@ -0,0 +1,73 @@
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-routing-machine'
+import L from 'leaflet'
+
+const customIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // URL de la imagen del ícono
+  iconSize: [32, 32], // Tamaño del ícono [ancho, alto]
+  iconAnchor: [16, 32], // Punto del ícono que se anclará al mapa [x, y]
+  popupAnchor: [0, -32] // Punto donde aparecerá el popup [x, y]
+})
+
+const startPoint = [37.18817, -3.60667] // Punto inicial (Granada)
+const mediunPoint = [37.1821, -3.60367] // Punto inicial (Granada)
+const endPoint = [37.166, -3.60] // Punto final
+
+function Routing() {
+  const map = useMap()
+
+  useEffect(() => {
+    if (!map) return
+
+    // Crear el control de rutas
+    const routingControl = L.Routing.control({
+      waypoints: [
+        L.latLng(startPoint), // Punto inicial
+        L.latLng(mediunPoint), // Punto intermedio
+        L.latLng(endPoint) // Punto final
+      ],
+      routeWhileDragging: true,
+      lineOptions: {
+        styles: [{ color: '#F47384', weight: 4 }]
+      },
+      createMarker: function() {
+        return null
+      }
+    }).addTo(map)
+
+    return () => {
+      map.removeControl(routingControl)
+    }
+  }, [map])
+
+  return null
+}
+
+function Map() {
+  return (
+    <MapContainer
+      center={startPoint}
+      zoom={14}
+      style={{ height: '98vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={startPoint} icon={customIcon}>
+        <Popup>Punto de inicio</Popup>
+      </Marker>
+      <Marker position={mediunPoint} icon={customIcon}>
+        <Popup>Punto de inicio</Popup>
+      </Marker>
+      <Marker position={endPoint} icon={customIcon}>
+        <Popup>Punto de fin</Popup>
+      </Marker>
+      <Routing />
+    </MapContainer>
+  )
+}
+
+export default Map
