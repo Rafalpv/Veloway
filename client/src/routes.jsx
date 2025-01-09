ccb02d246ee28e49b6c5ecc3a2b807186044bfb4
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2025-01-09 22:56:20 +0100

    feat: Install and configure TailwindCSS

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,8 +2,13 @@ import { createBrowserRouter } from 'react-router'
 import Login from './pages/Login'
 import Map from './pages/Map'
 import SignUp from './pages/SignUp'
+import App from './App'
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
   {
     path: '/login',
     element: <Login />
