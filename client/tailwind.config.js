ada8aa897349d0abf2519c02bdc2d8622e3dd2ec
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2025-01-15 03:06:32 +0100

    feat(signup/client) : Design and style the signup page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     './index.html',
     './src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'background-light': '#ECE8E8',
+        green: '#205435',
+        gold: '#FFC107',
+        'gold-dark': '#FFA000'
+
+      },
+      fontFamily: {
+        sixcaps: ['Six Caps', 'mono'],
+        montserrat: ['Montserrat', 'mono']
+      },
+      boxShadow: {
+        '3xl': '10px 10px 7px 3px rgba(51,51,51,0.92)'
+      }
+    }
   },
   plugins: []
 }
