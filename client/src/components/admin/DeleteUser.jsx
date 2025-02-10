0e6d8cbdf1553a26352b393c83d9da810a72a039
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2025-02-10 18:29:48 +0100

    feat(client) : Implement user deletion funtionality

---
+++ client/src/components/admin/DeleteUser.jsx
@@ -0,0 +1,55 @@
+import { useContext } from 'react'
+import { UsersContext } from '../../context/UsersContext'
+import { TiDelete } from 'react-icons/ti'
+import toast from 'react-hot-toast'
+
+const DeleteUser = ({ nickname }) => {
+  const { deleteUser } = useContext(UsersContext)
+
+  const handleDeleteUser = () => {
+    toast((t) => (
+      <div className="rounded-lg flex flex-col items-center text-center">
+        <p className="text-lg font-semibold mb-2 text-gray-800">
+          ¿Eliminar usuario <span className="font-bold text-red-600">{nickname}</span>?
+        </p>
+        <div className="flex gap-3 mt-3">
+          <button
+            onClick={() => {
+              deleteUser(nickname)
+              toast.dismiss(t.id)
+            }}
+            className="px-4 py-2 bg-red-500 text-white font-bold rounded-md hover:bg-red-700 transition"
+          >
+            Eliminar
+          </button>
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="px-4 py-2 bg-gray-300 text-gray-800 font-bold rounded-md hover:bg-gray-400 transition"
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+    ), {
+      duration: 4000,
+      style: {
+        minWidth: '100px',
+        background: '#DFDED3',
+        border: '2px solid black',
+        padding: '16px'
+
+      }
+    })
+  }
+
+  return (
+    <button
+      className="justify-between px-4 py-2 border-2 border-black shadow-boton rounded-full hover:bg-red-400 duration-300"
+      onClick={handleDeleteUser}
+    >
+      <TiDelete className="text-4xl cursor-pointer" />
+    </button>
+  )
+}
+
+export default DeleteUser
