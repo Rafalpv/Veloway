3bc6e2f30041a5762ad91be0cb2658c2ebcd3d2a
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2025-04-03 15:29:29 +0200

    feat(client) : Add resume of the route before the procces of save

--- client/src/modules/createRoute/components/SaveRouteButton.jsx
+++ client/src/modules/createRoute/components/SaveRouteButton.jsx
@@ -4,6 +4,43 @@ import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 import { useMapMarkers } from '@user/context/MapMarkersContext'
 
+const ResumeStepsByLeg = ({ leg, index }) => {
+  const [infoSteps, setInfoSteps] = useState(false)
+
+  return (
+    <div key={index} className="p-4 border rounded-lg bg-gray-100 shadow-md">
+      {/* Encabezado con la flecha alineada a la derecha */}
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="text-md font-semibold text-gray-800">
+            {leg.start_address} → {leg.end_address}
+          </p>
+          <p className="text-sm text-gray-600">
+            {leg.distance.text} | {leg.duration.text}
+          </p>
+        </div>
+        <MdOutlineKeyboardArrowDown
+          size={30}
+          className={`cursor-pointer transition-transform ${infoSteps ? 'rotate-180' : ''
+            }`}
+          onClick={() => setInfoSteps(!infoSteps)}
+        />
+      </div>
+
+      {/* Lista de pasos con animación */}
+      {infoSteps && (
+        <ul className="mt-3 bg-white p-3 rounded-md shadow-md border border-gray-200">
+          {leg.steps.map((step, stepIndex) => (
+            <li key={stepIndex} className="text-sm text-gray-700 mb-1 border-l-4 border-blue-500 pl-2">
+              {step.html_instructions.replace(/<[^>]+>/g, '')}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
 const SaveRouteButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [routeName, setRouteName] = useState('')
@@ -42,20 +79,7 @@ const SaveRouteButton = () => {
             />
             <div className='mb-6 space-y-4'>
               {legs.map((leg, index) => (
-                <div key={index} className='p-4 border rounded-lg bg-gray-100'>
-                  <p className='text-md font-semibold text-gray-800'>
-                    {leg.start_address} → {leg.end_address}
-                  </p>
-                  <p className='text-sm text-gray-600'>
-                    {leg.distance.text} | {leg.duration.text}
-                  </p>
-                  {/* <ul className='list-disc list-inside text-xs text-gray-700 mt-2'>
-                    {leg.steps.map((step, stepIndex) => (
-                      <li key={stepIndex}>{step.instructions.replace(/<[^>]+>/g, '')}</li>
-                    ))}
-                  </ul> */}
-                  <MdOutlineKeyboardArrowDown size={30} />
-                </div>
+                <ResumeStepsByLeg leg={leg} key={index} />
               ))}
             </div>
 
